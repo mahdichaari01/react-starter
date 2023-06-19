76a8be5d1f6327b490fc48ada3ad238b836ab630
Mahdi Chaari <mahdi125@outlook.com>
2023-06-19 14:45:09 +0100

    initial commit

---
+++ src/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://some-domain.com/api/",
+  timeout: 1000,
+  headers: { "X-Custom-Header": "foobar" },
+});
+
+instance.interceptors.request.use((request) => {
+  console.log("request sent", request);
+  return request;
+});
+
+instance.interceptors.response.use((response) => {
+  console.log("response arrived", response);
+  return response;
+});
+
+export { instance };
